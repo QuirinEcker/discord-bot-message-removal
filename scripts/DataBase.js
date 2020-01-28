91af83126cf3930bda94dc6f033839c1d0dd1bcf
Quirin <quirin.ecker@bajupa.com>
2020-01-28 08:37:12 +0100

    implemented DataBase.js

---
+++ scripts/DataBase.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+class DataBase {
+
+    constructor(host, user, password) {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+    }
+
+    createTable(tableName) {
+
+    }
+}
