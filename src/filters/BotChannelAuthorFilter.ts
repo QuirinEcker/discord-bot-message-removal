932a4d22a6237939ed022f1ae000928c472ac807
Quirin <quirin.ecker@bajupa.com>
2020-04-05 18:50:28 +0200

    added respond for filters

--- src/filters/BotChannelAuthorFilter.ts
+++ src/filters/BotChannelAuthorFilter.ts
@@ -4,6 +4,10 @@ import {Message} from "eris";
 const filterConf = require("../../config/filterConf.json");
 
 export class BotChannelAuthorFilter extends Filter{
+    constructor() {
+        super("f");
+    }
+
     protected check(msg: Message): boolean {
         return msg.channel.id === filterConf.botChannelID;
     }
--- src/filters/Filter.ts
+++ src/filters/Filter.ts
@@ -1,12 +1,22 @@
 import {Message} from "eris";
 
 export abstract class Filter {
+    protected respond: string;
+
+    constructor(respnd: string) {
+        this.respond = respnd;
+    }
 
     protected abstract check(msg: Message): boolean;
 
     public execute(msg: Message) {
         if (this.check(msg)) {
             msg.delete();
+            this.sendRespond(msg)
         }
+    }
+
+    protected async sendRespond(msg: Message) {
+        await msg.channel.createMessage(this.respond)
     }
 }
