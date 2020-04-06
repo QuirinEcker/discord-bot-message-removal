a5874ad2b755dbd40dd7922ad33dcfdca4d7b7e2
Quirin <quirin.ecker@bajupa.com>
2020-04-06 17:15:50 +0200

    implemented CommandFilter Construct

---
+++ src/filters/CommandFilter.ts
@@ -0,0 +1,22 @@
+import {Filter} from "./Filter";
+import {Message, TextChannel} from "eris";
+
+const filterConf = require("../../ServerConfigs.json");
+
+export class CommandFilter extends Filter{
+    constructor() {
+        super(true);
+    }
+
+    protected filterCheck(msg: Message): boolean {
+        return false;
+    }
+
+    protected sendRespond(msg: Message) {
+        if (msg.channel instanceof TextChannel) {
+            msg.channel.createMessage(
+                `Bot Commands belong in the <#${filterConf[msg.channel.guild.id].botChannelID}>`
+            ).catch(console.log);
+        }
+    }
+}
