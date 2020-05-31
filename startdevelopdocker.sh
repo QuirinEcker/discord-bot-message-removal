2f51a1058f8d390c6c6afcc21f2d1135a47d8801
Quirin <quirin.ecker@bajupa.com>
2020-05-31 20:54:56 +0200

    removed files with no use

--- startdevelopdocker.sh
+++
@@ -1,8 +0,0 @@
-cp ./package.json ./docker/docker-develop/node/package.json
-cp ./package-lock.json ./docker/docker-develop/node/package-lock.json
-
-cd docker/docker-develop/
-docker-compose up -d --build
-rm node/package.json
-rm node/package-lock.json
-cd ..
